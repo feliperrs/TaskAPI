1dc34a38ac2aadec185d0cff18e4c2c1b701789a
Felipe Ribeiro Rodrigues dos Santos <feliperrsdev@gmail.com>
2025-08-20 09:23:03 -0300

    Criação do banco de dados e correção da função POST

--- task-api/src/main/java/com/example/task_api/controller/TarefaController.java
+++ task-api/src/main/java/com/example/task_api/controller/TarefaController.java
@@ -8,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,13 +25,14 @@ public class TarefaController {
     private TarefaRepository tarefaRepository;
 
     // Cria uma tarefa
+    @PostMapping
     public Tarefa criarTarefa(@RequestBody Tarefa tarefa) {
         return tarefaRepository.save(tarefa);
     }
 
     // Lista todas as terefas
     @GetMapping
-    public List<Tarefa> listarTarefas() {
+    public List<Tarefa> listarTarefas() {	
         return tarefaRepository.findAll();
     }
 
